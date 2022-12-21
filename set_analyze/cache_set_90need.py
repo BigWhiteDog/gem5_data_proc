7d82f2c9b8dfa4735631c559e52ea4ec636c26da
BigWhiteDog <legend.z@foxmail.com>
2022-12-21 16:58:35 +0800

    add draw for est way need with mr

--- set_analyze/cache_set_90need.py
+++ set_analyze/cache_set_90need.py
@@ -66,6 +66,46 @@ def draw_one_workload_way_need(ax,s_dicts,workload_name,full_ass,pos:tuple):
         # ax.legend(shadow=0, fontsize = 12, bbox_to_anchor=(-0.01,1.3,0,0), loc = 'upper left',  \
         #     borderaxespad=0.2, ncol = 10, columnspacing=0.5, labelspacing=0.1)
 
+def draw_one_workload_way_need_withmr(ax,s_dicts,workload_name,full_ass,pos:tuple):
+    label_s = ['min_ways_no_extra_miss','min_ways_1_extra_miss', 'min_ways_2_extra_miss','full_ways_miss_rate']
+    extra0_list = s_dicts[label_s[0]]
+    extra1_list = s_dicts[label_s[1]]
+    extra2_list = s_dicts[label_s[2]]
+    full_mr_list = s_dicts[label_s[3]]
+    sorted_setlist = sorted(zip(extra0_list,full_mr_list,extra1_list,extra2_list))
+    s_extra0_list,s_full_mr_list,s_extra1_list,s_extra2_list = zip(*sorted_setlist)
+    x_val = np.arange(all_set)
+    full_ass_vals = np.full(all_set,full_ass)
+
+    extra0_list_color = contrasting_orange[6]
+    extra1_list_color = contrasting_orange[7]
+    extra2_list_color = contrasting_orange[8]
+    alpha_set = 0.8
+    # ax.plot(s_extra2_list, label='min ways 2 extra miss', color = extra2_list_color,linewidth=1)
+    # ax.fill_between(x_val,full_ass_vals, s_extra2_list, color = extra2_list_color, alpha=alpha_set)
+    # ax.plot(s_extra1_list, label='min ways 1 extra miss', color = extra1_list_color,linewidth=1)
+    # ax.fill_between(x_val, full_ass_vals, s_extra1_list, color = extra1_list_color, alpha=alpha_set)
+    ax.plot(s_extra0_list, label='min ways no extra miss', color = extra0_list_color,linewidth=1)
+    ax.fill_between(x_val, full_ass_vals, s_extra0_list, color = extra0_list_color, alpha=alpha_set)
+    ax.set_ylabel('needed ways')
+    ax.set_ylim(0, 8)
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    
+    ax2 = ax.twinx()
+    ax2.plot(s_full_mr_list, label='full ways miss rate', color = contrasting_orange[9],linewidth=2)
+    ax2.set_ylabel('miss rate of the set')
+    ax2.set_ylim(0, 1)
+    ax2.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1, decimals=0))
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+
+    ax.set_xlabel('set idx (sorted by min 0miss ways)')
+    ax.set_title(f'{workload_name}')
+    if pos == (0,0):
+        ax.legend(shadow=0, fontsize = 13, bbox_to_anchor=(-0.01,1.4), loc = 'upper left',  \
+            borderaxespad=0.2, ncol = 1, columnspacing=0.5, labelspacing=0.1)
+        # ax.legend(shadow=0, fontsize = 12, bbox_to_anchor=(-0.01,1.3,0,0), loc = 'upper left',  \
+        #     borderaxespad=0.2, ncol = 10, columnspacing=0.5, labelspacing=0.1)
+
 
 def draw_db_by_func(base_dir,n_rows,worksname_waydict,draw_one_func,fig_name):
     fig,ax = plt.subplots(n_rows,4)
@@ -85,6 +125,7 @@ def draw_db_by_func(base_dir,n_rows,worksname_waydict,draw_one_func,fig_name):
         s_dicts['unique_blocks_number'] = [0 for _ in range(all_set)]
         s_dicts['unique_reused_blocks_number'] = [0 for _ in range(all_set)]
         s_dicts['ways_miss_cnt'] = {}
+        s_dicts['ways_miss_rate'] = {}
 
         partsname = os.listdir(word_dir) #like l3-1
         for part in partsname:
@@ -101,16 +142,19 @@ def draw_db_by_func(base_dir,n_rows,worksname_waydict,draw_one_func,fig_name):
 
             new_base = os.path.join(word_dir,part)
             db_path = os.path.join(new_base,'hm.db')
-            all_access_query = 'SELECT SETIDX,sum(ISMISS) FROM HitMissTrace group by SETIDX,TAG;'
+            all_access_query = 'SELECT SETIDX,sum(ISMISS),count(*) FROM HitMissTrace group by SETIDX;'
             con = sqlite3.connect(db_path)
             cur = con.cursor()
             f = cur.execute(all_access_query)
 
             s_dicts['ways_miss_cnt'][ways] = [0 for _ in range(all_set)]
-            for setidx,msc in f:
+            s_dicts['ways_miss_rate'][ways] = [0 for _ in range(all_set)]
+            for setidx,msc,allaccess in f:
                 idx = int(setidx)
                 msc = int(msc)
-                s_dicts['ways_miss_cnt'][ways][idx] += msc
+                allaccess = int(allaccess)
+                s_dicts['ways_miss_cnt'][ways][idx] = msc
+                s_dicts['ways_miss_rate'][ways][idx] = msc/allaccess
 
             cur.close()
         s_dicts['min_ways_no_extra_miss'] = [full_ass for _ in range(all_set)]
